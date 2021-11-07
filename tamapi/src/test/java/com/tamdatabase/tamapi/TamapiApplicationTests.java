beccea88f47d80c4dfbfc37f30a3c71809c287e8
Ricardo Ortega <tattortega.28@gmail.com>
2021-11-07 18:31:52 -0500

    API
    Estructura del API Rest en la que se trabajara para el proyecto

---
+++ tamapi/src/test/java/com/tamdatabase/tamapi/TamapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamdatabase.tamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
