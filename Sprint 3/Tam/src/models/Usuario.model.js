6e12914c945897bde9795a6ac2f53fd29649c7fa
NeomuraDG <89641327+NeomuraDG@users.noreply.github.com>
2021-11-20 23:44:17 -0500

    Update Usuario.model.js
    Elimine fecha de actualización

--- Sprint 3/Tam/src/models/Usuario.model.js
+++ Sprint 3/Tam/src/models/Usuario.model.js
@@ -9,20 +9,9 @@ const UsuarioSchema = new Schema({
     roles_id:String,
     usuario:{type:String, required:[true, 'Campo obligatorio']},
     contraseña:{type:String, required:[true, 'Campo obligatorio']},
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    created_at: {type: Date, default: Date.now}
 })
 
-// Sets the created_at parameter equal to the current time
-/*UsuarioSchema.pre('save', async function(){
-    now = new Date();
-    this.updated_at = now;
-    if(!this.created_at) {
-        this.created_at = now
-    }
-    await doStuff();
-});
-*/
 
 //convertir a modelo
 
