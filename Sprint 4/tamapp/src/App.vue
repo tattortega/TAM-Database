f524e3c508c5717d72a5ff06e4580298ab3fa12e
Ricardo Ortega <tattortega.28@gmail.com>
2021-11-28 15:23:11 -0500

    Proyecto vue

---
+++ Sprint 4/tamapp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <Header />
+    <NavBar />
+
+    <v-main> 
+      <router-view/>
+    </v-main>
+    <Footer />
+
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+export default {
+  name: 'App',
+
+  data: () => ({
+      //
+  }),
+  components: {
+    Header,
+    NavBar,
+    Footer,
+  }
+};
+</script>
---
+++ Sprint 4/tamapp/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <Login/>
+</template>
+
+<script>
+import Login from "./Login.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Login,
+  },
+};
+</script>
