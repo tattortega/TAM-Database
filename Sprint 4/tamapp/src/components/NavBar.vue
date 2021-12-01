35902553471b0c70f8ea3cedccebb255d1959dc1
Ricardo Ortega <tattortega.28@gmail.com>
2021-11-30 20:09:50 -0500

    actualizar navbar

--- Sprint 4/tamapp/src/components/NavBar.vue
+++ Sprint 4/tamapp/src/components/NavBar.vue
@@ -1,46 +1,60 @@
 <template>
-  <nav class="MenuWrap">
-    <div class="menu-item">
-      <a href="../FormularioContactarSoporte/contactar_soporte.html" class="ListItem">Contáctanos</a>
-    </div>
-    <div class="menu-item">
-      <a href="../FormularioInformes/informes.html" class="ListItem">Consultas</a>
-    </div>
-  </nav>
+    <nav class="MenuWrap">
+        <div class="menu-item">
+            <a href="../Index/index.html" class="ListItem">Cerrar sesión</a>
+        </div>
+        <div class="menu-item">
+            <a href="../FormularioContactarSoporte/contactar_soporte.html" class="ListItem">Contáctanos</a>
+        </div>
+        <div class="menu-item">
+            <a href="../FormularioInformes/informes.html" class="ListItem">Consultas</a>
+        </div>
+        <div class="menu-item">
+            <a href="../FormularioIngresoDatos/IngresoDatos.html" class="ListItem">Ingresa tus datos</a>
+        </div>        
+    </nav>
 </template>
 
+<script>
+export default {
+ data: function(){
+   return{
 
+   }
+ }
+}
+</script>
 
 <style>
-.MenuWrap {
-  background-color: indigo;
-  font-family: "Oswald", sans-serif;
-  font-size: 1rem;
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: center;
-}
+  
+  .MenuWrap {
+    background-color: indigo;
+    font-family: 'Oswald', sans-serif;
+    font-size: 1rem;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+  }
 
-.menu-item {
-  border-left: #ebebeb solid;
-  padding: 0.5em;
-  cursor: pointer;
-}
+  .menu-item{
+    border-left: #ebebeb solid;
+    padding: 0.5em;
+  }
 
-.ListItem,
-.LastItem {
-  color: #ebebeb;
-  text-decoration: none;
-}
-.ListItem {
-  margin-right: 1rem;
-}
-.LastItem {
-  margin-right: auto;
-}
+  .ListItem,
+  .LastItem {
+    color: #ebebeb;
+    text-decoration: none;
+  }
+  .ListItem {
+    margin-right: 1rem;
+  }
+  .LastItem {
+    margin-right: auto;
+  }
 
 /*Sombrear de amarillo las opciones de nav (barra de menús)*/
-.menu-item a:hover {
-  color: rgb(226, 238, 55);
-}
+  a:hover{
+    color: rgb(226, 238, 55);
+  }
 </style>
