c7b3f22dbeb859ec3fb5deab3b4d0a68d4be1d6e
Ricardo Ortega <tattortega.28@gmail.com>
2021-12-07 23:07:24 -0500

    update api

--- Sprint 3/Tam/src/index.js
+++ Sprint 3/Tam/src/index.js
@@ -4,9 +4,20 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyparser = require('body-parser')
+require('dotenv').config()
 require('./database')
 
 //voy a configurar el puerto
+app.use(cors({origen:'*'}))
+app.use((req, res, next) => {
+    // Dominio que tengan acceso (ej. 'http://example.com')
+       res.setHeader('Access-Control-Allow-Origin', '*');    
+    // Metodos de solicitud que deseas permitir
+       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');    
+    // Encabecedados que permites (ej. 'X-Requested-With,content-type')
+       res.setHeader('Access-Control-Allow-Headers', '*');    
+    next();
+})
 
 app.set('Port', process.env.PORT || 4000)
 app.use(morgan('dev'))
