fa601c1323fceaa6835fc5b9f0face7db2bc46fd
Ricardo Ortega <tattortega.28@gmail.com>
2021-12-08 05:51:58 -0500

    Update index.js

--- Sprint 3/Tam/src/index.js
+++ Sprint 3/Tam/src/index.js
@@ -4,26 +4,26 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyparser = require('body-parser')
-require('dotenv').config()
+// require('dotenv').config()
 require('./database')
 
 //voy a configurar el puerto
 app.use(cors({origen:'*'}))
-app.use((req, res, next) => {
-    // Dominio que tengan acceso (ej. 'http://example.com')
-       res.setHeader('Access-Control-Allow-Origin', '*');    
-    // Metodos de solicitud que deseas permitir
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');    
-    // Encabecedados que permites (ej. 'X-Requested-With,content-type')
-       res.setHeader('Access-Control-Allow-Headers', '*');    
-    next();
-})
+// app.use((req, res, next) => {
+//     // Dominio que tengan acceso (ej. 'http://example.com')
+//        res.setHeader('Access-Control-Allow-Origin', '*');    
+//     // Metodos de solicitud que deseas permitir
+//        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');    
+//     // Encabecedados que permites (ej. 'X-Requested-With,content-type')
+//        res.setHeader('Access-Control-Allow-Headers', '*');    
+//     next();
+// })
 
 app.set('Port', process.env.PORT || 4000)
 app.use(morgan('dev'))
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json())
-app.use(cors({origen:'*'}))
+// app.use(cors({origen:'*'}))
 
 app.use(require('./routes/usuario.route'))
 app.use(require('./routes/soporte.route'))
